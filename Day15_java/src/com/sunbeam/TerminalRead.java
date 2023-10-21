7ef5474ca83c053f9979450bda934a25bccb3d0d
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-10-21 21:20:57 +0530

    Assignment15

---
+++ Day15_java/src/com/sunbeam/TerminalRead.java
@@ -0,0 +1,31 @@
+package com.sunbeam;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+
+public class TerminalRead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try(FileReader fin=new FileReader("Buffer.txt")){
+			try(BufferedReader brd =new BufferedReader(fin)){
+				while(true) {
+					String line = brd.readLine();
+					if(line==null) {
+						break;
+					}
+					System.out.println(line);
+				}
+			}
+			
+		}
+		catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+
+	}
+
+}
+
