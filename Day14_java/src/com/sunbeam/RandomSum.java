c32ca6e2eec86fd34e51254c4c8f4894a212f8d6
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2023-10-21 17:54:47 +0530

    Assignment14

---
+++ Day14_java/src/com/sunbeam/RandomSum.java
@@ -0,0 +1,18 @@
+package com.sunbeam;
+
+import java.util.Random;
+import java.util.stream.Stream;
+
+
+public class RandomSum {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Random r=new Random();
+		Stream<Integer> strm= Stream.generate(()->r.nextInt(10)).limit(10);
+		int res=strm.reduce(0, (a,b)->a+b);
+		System.out.println("Result: "+res);
+	}
+
+}
